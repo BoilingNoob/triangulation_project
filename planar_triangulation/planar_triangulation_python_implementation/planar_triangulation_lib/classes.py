81382e5f4a2e3062b66e5470740c6d9b6c1dd74f
BoilingNoob <morgandowd@teksavvy.com>
2021-09-11 21:05:26 -0400

    added class for passing data with 3 measures and an original location

--- planar_triangulation/planar_triangulation_python_implementation/planar_triangulation_lib/classes.py
+++ planar_triangulation/planar_triangulation_python_implementation/planar_triangulation_lib/classes.py
@@ -6,3 +6,18 @@ class Measurment:
     y: float
     z: float
     radius: float
+
+
+class data_packet:
+    measurement1 = None
+    measurement2 = None
+    measurement3 = None
+    location = None
+
+    def __init__(self,measurement1,measurement2,measurement3,location):
+        self.measurement1 = measurement1
+        self.measurement2 = measurement2
+        self.measurement3 = measurement3
+        self.location = location
+        
+
